b401ea86a3e795b649c583c396abaaa851eaa5d2
prachinimje <prachinimje1995@gmail.com>
2021-07-04 20:41:37 +0530

    adding new project

---
+++ src/main/java/com/jbk/Demo1.java
@@ -0,0 +1,18 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void test01(){
+		System.out.println("Hello.....");
+	}
+	
+	@Test
+	public void test02(){
+		System.out.println("Hii.....");
+	}
+
+
+}
